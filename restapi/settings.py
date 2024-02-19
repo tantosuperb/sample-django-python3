a1b5b39dee1259f6674a58d11d19eff53510b5da
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-02-19 15:42:06 +0900

    feat: 단일 실행용 sqlite3 설정 추가, 초기값 추가

--- restapi/settings.py
+++ restapi/settings.py
@@ -79,14 +79,34 @@ WSGI_APPLICATION = 'restapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project_db',
-        'USER': 'root',
-        'PASSWORD': 'password1!',
-        'HOST': '10.78.95.55',
-        # 'HOST': '192.168.1.29',
-        'PORT': '13306', # MySQL
-        # 'PORT': '13307', # MariaDB
+        # InMemory sqlite3
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
+        # docker-compose MySQL
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'project_db',
+        # 'USER': 'root',
+        # 'PASSWORD': 'password1!',
+        # 'HOST': '10.78.95.55',
+        # # 'HOST': '192.168.1.29',
+        # 'PORT': '13306',
+
+        # docker-compose MariaDB
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'project_db',
+        # 'USER': 'root',
+        # 'PASSWORD': 'password1!',
+        # 'HOST': '10.78.95.55',
+        # 'PORT': '13307',
+
+        # k8s Core DNS MariaDB
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'project_db',
+        # 'USER': 'root',
+        # 'PASSWORD': 'password1!',
+        # 'HOST': 'app-d39a8e1f53ce74efc5c189686f9d92d5-chart.50010-9cdcd4.svc.cluster.local',
+        # 'PORT': '3306',
     }
 }
 
--- runScript.sh
+++ runScript.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-BUILD_NAME=sample-django-api-db
+BUILD_NAME=sample-django-python3
 
 docker stop $BUILD_NAME
 docker rmi $BUILD_NAME
 docker build -t $BUILD_NAME . 2>&1 || exit
 
 # foreground -> script exit = docker process stop
-#docker run --rm -p 8000:8000 -v "$(pwd)"/sample-vol:/app --name $BUILD_NAME $BUILD_NAME
-docker run --rm -p 8000:8000 --name $BUILD_NAME $BUILD_NAME
+#docker run --rm -p 8000:8000 --name $BUILD_NAME $BUILD_NAME
+
+# for docker data remaining local
+docker run --rm -p 8000:8000 --name $BUILD_NAME -v ./:/app $BUILD_NAME
 
 # background -> must docker stop manually for process exit
 # docker run -d --rm -p 8000:8000 --name $BUILD_NAME $BUILD_NAME
